44d0254d48296dca53b3f761687efb3251ac3aa2
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-06-02 10:27:06 +0300

    set up convex and addex podcast and user schema

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,42 @@
+"use client";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+import PodcastCard from '@/components/PodcastCard'
 import { Button } from '@/components/ui/button'
+import { podcastData } from '@/constants'
 import React from 'react'
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
+
   return (
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>
           Trending Podcasts
         </h1>
-        {/* <Button className='text-white-1 bg-orange-1'>Button</Button> */}
+
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </main>
+
+        <div className='podcast_grid'>
+          {
+            podcastData.map(({ id, title, description, imgURL }) => {
+              return (
+                <PodcastCard
+                  key={id}
+                  title={title}
+                  description={description}
+                  imgURL={imgURL}
+                  podcastId={id}
+
+                />
+              )
+            })
+          }
+        </div>
       </section>
     </div>
   )
---
+++ components/RightSideBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RightSideBar = () => {
+  return (
+  <section className='right_sidebar'>
+   <h1 className='text-20 font-bold text-white-1'>
+          Right Side Bar
+        </h1>
+  </section>
+  )
+}
+
+export default RightSideBar
