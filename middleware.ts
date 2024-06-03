e8447d875d046023bf0c30ee74ee9b08865e5512
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-06-03 13:23:07 +0300

    implemented signin and signup with clerk

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// const isDashboardRoute = createRouteMatcher(['/dashboard(.*)']);
-// const isAdminRoute = createRouteMatcher(['/admin(.*)']);
-const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up', '/'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
 
 export default clerkMiddleware((auth, req) => {
-  if(!isPublicRoute(req)) auth().protect();
-
-  // Restrict admin route to users with specific role
-  // if (isAdminRoute(req)) auth().protect({ role: 'org:admin' });
-
-  // Restrict dashboard routes to signed in users
-  // if (isDashboardRoute(req)) auth().protect();
+  if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
