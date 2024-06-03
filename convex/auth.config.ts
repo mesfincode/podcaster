1c36e086ce3f6a14f508945dc1aaed937cc814ef
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-06-03 09:01:41 +0300

    created user schema

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+  const authConfig= {
+    providers: [
+      {
+        domain: "https://romantic-anteater-69.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
